70fb823f0b5914dd6ff1cd4bc205ae807c741ec2
Frank Binder <github@serialize.eu>
2014-05-18 13:29:53 +0200

    refactoring

--- mkimage/mkimage.sh
+++ mkimage/mkimage.sh
@@ -3,10 +3,18 @@
 set -e -u -o pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DEFAULT_REPO_URL="http://mirrors.kernel.org/archlinux"
 DEFAULT_ARCH=`uname -m`
+DEFAULT_REPO_URL="http://mirrors.kernel.org/archlinux"
+DEFAULT_CACHE_DIR=$SCRIPT_DIR/cache
 DEFAULT_BUILD_DIR=$SCRIPT_DIR/build
-REPO_CACHE=$SCRIPT_DIR/pkg
+
+ARCH="$DEFAULT_ARCH"
+REPO_URL="$DEFAULT_REPO_URL"
+REPO_CORE_URL="$REPO_URL/core/os/$ARCH"
+REPO_EXTRA_URL="$REPO_URL/extra/os/$ARCH"
+CACHE_DIR="$DEFAULT_CACHE_DIR"
+BUILD_DIR="$DEFAULT_BUILD_DIR"
+TMP_DIR="/tmp/"
 
 BASH_PACKAGES=(
    iana-etc filesystem linux-api-headers tzdata glibc ncurses readline bash
@@ -27,6 +35,16 @@ function debug() {
    stderr "--- $@"; 
 }
 
+# Output to standard error
+function stderr-n() { 
+   echo -n "$@" >&2; 
+}
+
+# Output debug message to standard error
+function debug-n() { 
+   stderr-n "$@"; 
+}
+
 # Extract href attribute from HTML link
 function extract_href() { 
    sed -n '/<a / s/^.*<a [^>]*href="\([^\"]*\)".*$/\1/p'; 
@@ -42,8 +60,8 @@ function uncompress() {
   local FILEPATH=$1 DEST=$2
   
   case "$FILEPATH" in
-    *.gz) tar xzf "$FILEPATH" -C "$DEST" --exclude="/usr/share/man";;
-    *.xz) xz -dc "$FILEPATH" | tar x -C "$DEST" --exclude="/usr/share/man";;
+    *.gz) res=$(tar xzf "$FILEPATH" -C "$DEST" --exclude="/usr/share/man" >&2);;
+    *.xz) res=$(xz -dc "$FILEPATH" | tar x -C "$DEST" --exclude="/usr/share/man" >&2);;
     *) debug "Error: unknown package format: $FILEPATH"
        return 1;;
   esac
@@ -52,35 +70,37 @@ function uncompress() {
 function fetch_packages_list() {
   local REPO=$1 
   
-  debug "fetch packages list: $REPO/"
   # Force trailing '/' needed by FTP servers.
   fetch -O - "$REPO/" | extract_href | awk -F"/" '{print $NF}' | sort -rn ||
     { debug "Error: cannot fetch packages list: $REPO"; return 1; }
 }
 
-function fetch_packages() {
-  local PACKAGES=$1 DEST=$2 LIST=$3 PACKDIR=$4
-  debug "fetching packages and uncompress: $PACKAGES"
+function _fetch_packages() {
+  debug " fetching package list..."
+  debug " "
+  local PACKAGES=$1 REPO=$2
   
+  local LIST=$(fetch_packages_list $REPO)
   for PACKAGE in $PACKAGES; do
+    debug-n "--- "  
     local FILE=$(echo "$LIST" | grep -m1 "^$PACKAGE-[[:digit:]].*\(\.gz\|\.xz\)$")
     test "$FILE" || { debug "Error: cannot find package: $PACKAGE"; return 1; }
-    local FILEPATH="$PACKDIR/$FILE"
-    if [ ! -d $REPO_CACHE ];then
-       mkdir $REPO_CACHE
-    fi
-    if [ -f $REPO_CACHE/$FILE ];then
-       cp $REPO_CACHE/$FILE $PACKDIR/
+    debug-n " $FILE --> "
+       
+    local TMP_PATH="$TMP_DIR/$FILE" CACHE_PATH="$CACHE_DIR/$FILE" REPO_URI="$REPO/$FILE"
+    if [ -f $CACHE_PATH ];then
+       debug-n "[cache]" # $REPO/$FILE"
+       cp $CACHE_PATH $TMP_DIR/
     fi    
-    if [ ! -f $FILEPATH ];then
-       debug "download package: $REPO/$FILE"
-       fetch -O "$FILEPATH" "$REPO/$FILE"
-       cp $FILEPATH $REPO_CACHE/
+    if [ ! -f $TMP_PATH ];then
+       debug-n "[repo]" # $REPO/$FILE"
+       fetch -O "$TMP_PATH" "$REPO_URI"
+       cp $TMP_PATH $CACHE_DIR/
     fi
-    debug "uncompress package: $FILEPATH"
-    uncompress "$FILEPATH" "$DEST"
+    stderr " "
+    uncompress "$TMP_PATH" "$BUILD_DIR"
   done
-  rm_man "$DEST"
+  #rm_man "$BUILD_DIR"
 }
 
 function fetch_version() {
@@ -109,18 +129,57 @@ function docker_build() {
    docker build -t $name $path/.
 }
 
-function configure_system() {
-   local DEST=$1
+function prepare_image() {
+   
+   debug "-------------------------------------------------------------------"
+   debug " preparing image..."  
+   debug "-------------------------------------------------------------------"
+          
+   while getopts "a:r" ARG; do
+      case "$ARG" in
+         a) ARCH=$OPTARG;;
+         r) REPO_URL=$OPTARG;;
+         d) BUILD_DIR=$OPTARG;;
+      esac
+   done
+   
+   [ -d "$BUILD_DIR" ] && rm -rf $BUILD_DIR
+   mkdir "$BUILD_DIR"
+   [ -d "$CACHE_DIR" ] || mkdir "$CACHE_DIR"
+   TMP_DIR=$(mktemp -d)
+   trap "rm -rf '$TMP_DIR'" KILL TERM EXIT
 
-   mkdir -p "$DEST/dev"
-   echo "root:x:0:0:root:/root:/bin/bash" > "$DEST/etc/passwd" 
-   echo 'root:$1$GT9AUpJe$oXANVIjIzcnmOpY07iaGi/:14657::::::' > "$DEST/etc/shadow"
-   touch "$DEST/etc/group"
-   echo "arch-minimal" > "$DEST/etc/hostname"
+   debug " arch:                $ARCH"
+   debug " build directory:     $BUILD_DIR"
+   debug " cache directory:     $CACHE_DIR"
+   debug " temporary directory: $TMP_DIR"
+   debug " "
+   debug " core repository:     $REPO_CORE_URL"
+   _fetch_packages "${BASH_PACKAGES[*]} ${PACMAN_PACKAGES[*]}" "$REPO_CORE_URL"
+      
+   debug " "
+   debug " extra repository:     $REPO_EXTRA_URL"
+   _fetch_packages "${EXTRA_PACKAGES[*]}" "$REPO_EXTRA_URL"
+   
+}
+
+function configure_image() {
+   
+   debug "-------------------------------------------------------------------"
+   debug " configure image..."  
+   debug "-------------------------------------------------------------------"
+   debug " creating config files"
+   
+   mkdir -p "$BUILD_DIR/dev"
+   echo "root:x:0:0:root:/root:/bin/bash" > "$BUILD_DIR/etc/passwd" 
+   echo 'root:$1$GT9AUpJe$oXANVIjIzcnmOpY07iaGi/:14657::::::' > "$BUILD_DIR/etc/shadow"
+   touch "$BUILD_DIR/etc/group"
+   echo "arch-minimal" > "$BUILD_DIR/etc/hostname"
   
-   test -e "$DEST/etc/mtab" || echo "rootfs / rootfs rw 0 0" > "$DEST/etc/mtab"
+   debug " rebuilding dev"
+   test -e "$BUILD_DIR/etc/mtab" || echo "rootfs / rootfs rw 0 0" > "$BUILD_DIR/etc/mtab"
    # udev doesn't work in containers, rebuild /dev
-   DEV=$DEST/dev
+   DEV=$BUILD_DIR/dev
    rm -rf $DEV
    mkdir -p $DEV
    mknod -m 666 $DEV/null c 1 3
@@ -137,42 +196,67 @@ function configure_system() {
    mknod -m 666 $DEV/ptmx c 5 2
    ln -sf /proc/self/fd $DEV/fd
 
+   debug " configure dns"
+   cp "/etc/resolv.conf" "$BUILD_DIR/etc/resolv.conf"
+   debug " configure pacman"
+   sed -i "s/^[[:space:]]*\(CheckSpace\)/# \1/" "$BUILD_DIR/etc/pacman.conf"
+   sed -i "s/^[[:space:]]*SigLevel[[:space:]]*=.*$/SigLevel = Never/" "$BUILD_DIR/etc/pacman.conf"
+   mkdir -p "$BUILD_DIR/etc/pacman.d/"
+   echo "Server = $REPO_URL/\$repo/os/$ARCH" >> "$BUILD_DIR/etc/pacman.d/mirrorlist"
+}
+
+function build_image() {
+   
+   debug "-------------------------------------------------------------------"
+   debug "building image..."  
+   debug "-------------------------------------------------------------------"
+  
+   local PACKAGES="${BASH_PACKAGES[*]} ${PACMAN_PACKAGES[*]} ${EXTRA_PACKAGES[*]}"
+   debug "installing packages..."
+   LC_ALL=C chroot "$BUILD_DIR" /usr/bin/pacman --noconfirm --arch $ARCH -Sy --force $PACKAGES
      
-  sed -i "s/^[[:space:]]*\(CheckSpace\)/# \1/" "$DEST/etc/pacman.conf"
-  sed -i "s/^[[:space:]]*SigLevel[[:space:]]*=.*$/SigLevel = Never/" "$DEST/etc/pacman.conf"
+   debug " configure dns"
+   cp "/etc/resolv.conf" "$BUILD_DIR/etc/resolv.conf"
+   debug " configure pacman"
+   sed -i "s/^[[:space:]]*\(CheckSpace\)/# \1/" "$BUILD_DIR/etc/pacman.conf"
+   sed -i "s/^[[:space:]]*SigLevel[[:space:]]*=.*$/SigLevel = Never/" "$BUILD_DIR/etc/pacman.conf"
+   mkdir -p "$BUILD_DIR/etc/pacman.d/"
+   echo "Server = $REPO_URL/\$repo/os/$ARCH" >> "$BUILD_DIR/etc/pacman.d/mirrorlist"
+
+   debug "starting haveged and init pacman-key"
+   LC_ALL=C chroot "$BUILD_DIR" /usr/bin/haveged -w 1024; /usr/bin/pacman-key --init; /usr/bin/pacman-key --populate archlinux
+   debug "removing haveged"
+   LC_ALL=C chroot "$BUILD_DIR" /usr/bin/pacman -Rs --noconfirm haveged;
 }
 
-function configure_pacman() {
-  local DEST=$1 ARCH=$2
-  debug "configure DNS and pacman"
-  cp "/etc/resolv.conf" "$DEST/etc/resolv.conf"
-  mkdir -p "$DEST/etc/pacman.d/"
-  echo "Server = $REPO_URL/\$repo/os/$ARCH" >> "$DEST/etc/pacman.d/mirrorlist"
+function finalize_image() {
+   debug "cleaning cache & removing man pages"
+   LC_ALL=C chroot "$BUILD_DIR" /usr/bin/pacman -Scc --noconfirm 
+   rm -rf $BUILD_DIR/usr/share/man/*
+   [ -f "$BUILD_DIR/.PKGINFO" ] && rm -f "$BUILD_DIR/.PKGINFO"
+   [ -f "$BUILD_DIR/.MTREE" ] && rm -f "$BUILD_DIR/.MTREE"
+   [ -f "$BUILD_DIR/.INSTALL" ] && rm -f "$BUILD_DIR/.INSTALL"
+   rm -rf "$TMP_DIR"   
+   
+   debug "pack content"
+   output="$SCRIPT_DIR/arch-minimal.tar.xz"
+   tar --xz -f "$output" --numeric-owner -C "$BUILD_DIR" -c . 
+   
+   debug "-----------------------------------------------------------------------"
+   debug "finished"
+   debug "-----------------------------------------------------------------------"
+     
 }
-
-function install_packages() {
-  local ARCH=$1 DEST=$2 PACKAGES=$3
-  debug "install packages: $PACKAGES"
-  LC_ALL=C chroot "$DEST" /usr/bin/pacman --noconfirm --arch $ARCH -Sy --force $PACKAGES
-}
-
-function remove_unwanted_item() {
-  debug "cleaning cache & removing man pages"
-  LC_ALL=C chroot "$DEST" /usr/bin/pacman -Scc --noconfirm 
-  rm -rf $DEST/usr/share/man/*
-}
-
-function init_pacman_key() {
-  debug "starting haveged and init pacman-key"
-  LC_ALL=C chroot "$DEST" /usr/bin/haveged -w 1024; \
-                           /usr/bin/pacman-key --init; \
-                           /usr/bin/pacman-key --populate archlinux
-  debug "removing haveged"
-  LC_ALL=C chroot "$DEST" /usr/bin/pacman -Rs --noconfirm haveged;
-}
-
 
 function main() {
+   prepare_image
+   configure_image
+   build_image
+   finalize_image
+}
+
+
+function main_old() {
    local starttime=$(date +%s) ARCH=$DEFAULT_ARCH REPO_URL=$DEFAULT_REPO_URL DEST=$DEFAULT_BUILD_DIR
   
    while getopts "a:r" ARG; do
@@ -186,7 +270,7 @@ function main() {
    DEST=$DEFAULT_BUILD_DIR
    [ -d "$DEST" ] && rm -rf $DEST
    mkdir "$DEST"
-   [ -d "$REPO_CACHE" ] || mkdir "$REPO_CACHE"
+   [ -d "$CACHE_DIR" ] || mkdir "$CACHE_DIR"
 
    local REPO="${REPO_URL%/}/core/os/$ARCH"
    local PACKDIR=$(mktemp -d)
@@ -203,12 +287,12 @@ function main() {
    LIST=$(fetch_packages_list $REPO)
    fetch_packages "${EXTRA_PACKAGES[*]}" "$DEST" "$LIST" "$PACKDIR"
    
-   for FILE in $REPO_CACHE/*; do
+   for FILE in $CACHE_DIR/*; do
       cp "$FILE" "$DEST/var/cache/pacman/pkg/"
    done
 
    [ -d "$DEST/opt/container" ] || mkdir -p $DEST/opt/container
-   fetch_version "${BASH_PACKAGES[*]} ${PACMAN_PACKAGES[*]} ${EXTRA_PACKAGES[*]}" "$REPO_CACHE" "minimal">> $DEST/opt/container/pkglist.minimal
+   fetch_version "${BASH_PACKAGES[*]} ${PACMAN_PACKAGES[*]} ${EXTRA_PACKAGES[*]}" "$CACHE_DIR" "minimal">> $DEST/opt/container/pkglist.minimal
    
    configure_pacman "$DEST" "$ARCH"
    configure_system "$DEST"
