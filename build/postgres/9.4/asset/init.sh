fa3c2db24103bf9a76e25e5428ab3246629fdfe3
Frank Binder <github@serialize.eu>
2014-12-08 23:39:36 +0100

    add init.sh and build.sh

--- build/postgres/9.4/asset/init.sh
+++ build/postgres/9.4/asset/init.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 pg_ctlcluster 9.4 main start
 
-
 # Start PostgreSQL
 echo "Starting PostgreSQL..."
-su -c '/usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/data -c config_file=/etc/postgresql/postgresql.conf' postgres
+su -c '/usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main/ -c config_file=/etc/postgresql/9.4/main/postgresql.conf' postgres
 
