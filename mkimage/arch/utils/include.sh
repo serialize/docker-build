5f28ff92d0356519bf7a08444527c948c994058b
Frank Binder <github@serialize.eu>
2014-12-09 12:10:42 +0100

    Naming convention

--- mkimage/arch/utils/include.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-INCLUDE_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $INCLUDE_SCRIPT_DIR/common.sh
-source $INCLUDE_SCRIPT_DIR/filesystem.sh
-source $INCLUDE_SCRIPT_DIR/repository.sh
-
