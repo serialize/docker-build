5baaa4ca8b5e17559f522c7abfa85ae3ff97b885
Frank Binder <github@serialize.eu>
2014-12-09 01:06:47 +0100

    postgres repository key removed

--- build/debian/jessie/assets/bootstrap.sh
+++ build/debian/jessie/assets/bootstrap.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-apt-key adv --keyserver pool.sks-keyservers.net --recv-keys B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8 
-
 echo deb http://http.debian.net/debian jessie main contrib non-free > /etc/apt/sources.list.d/debian-jessie.list
 /bin/rm /etc/apt/sources.list 
 
 echo en_US.UTF-8 UTF-8 > /etc/locale.gen 
-export DEBIAN_FRONTEND=noninteractive LANG 
+export DEBIAN_FRONTEND=noninteractive LANG
+ 
 apt-get update 
 apt-get install -y --no-install-recommends apt-utils locales 
 
